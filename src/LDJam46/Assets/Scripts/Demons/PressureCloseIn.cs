4522de21e86232c317526e6157d91352d29083f1
SilasReinagel <silas.reinagel@gmail.com>
2020-04-18 01:23:06 -0700

    Created Close-In Demon. Closes #30

---
+++ src/LDJam46/Assets/Scripts/Demons/PressureCloseIn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class PressureCloseIn : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    [SerializeField] private float fullyOpenScale = 1.6f;
+    [SerializeField] private float fullyClosedScale = 0.002f;
+    [SerializeField] private float closeSpeed = 0.5f;
+    [SerializeField, Range(0f, 1f)] private float currentClosedAmount = 0f;
+
+    private void OnEnable() => currentClosedAmount = 0;
+    
+    private void Update()
+    {
+        currentClosedAmount += closeSpeed * Time.deltaTime;
+        var amount = Mathf.Lerp(fullyOpenScale, fullyClosedScale, currentClosedAmount);
+        image.rectTransform.localScale = new Vector3(amount, amount, 1);
+    }
+}
