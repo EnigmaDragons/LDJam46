ab12121f0621832b34c1112341d0168e4e900985
SilasReinagel <silas.reinagel@gmail.com>
2020-04-18 15:50:08 -0700

    Added Object Spawner. Closes #166

--- src/LDJam46/Assets/Scripts/Mind/MapGenerator.cs
+++ src/LDJam46/Assets/Scripts/Mind/MapGenerator.cs
@@ -5,11 +5,13 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] private int size;
     [SerializeField] private GroundSpawner ground;
     [SerializeField] private TreeSpawner trees;
+    [SerializeField] private ComfortSpawner comfort;
 
     public void OnEnable()
     {
         var sizeV = new Vector2Int(size, size);
         ground.Spawn(sizeV);
         trees.Spawn(sizeV);
+        comfort.Spawn(sizeV);
     }
 }
