ba2e18b685d20ab758f9068a48d77ce9ba6e80f9
Mark Arneman <markarneman@gmail.com>
2020-04-18 16:54:54 -0400

    Transition to thinking if idle for too long

--- src/LDJam46/Assets/Scripts/MainCharacter/Rigidbody2dMovement.cs
+++ src/LDJam46/Assets/Scripts/MainCharacter/Rigidbody2dMovement.cs
@@ -13,6 +13,9 @@ public class Rigidbody2dMovement : MonoBehaviour {
 
     private float _horizontal;
     private float _vertical;
+    private float _idleTimer;
+
+    private bool _isThinking = false;
 
     private void Awake()
     {
@@ -25,6 +28,23 @@ public class Rigidbody2dMovement : MonoBehaviour {
     {
         _horizontal = Input.GetAxisRaw("Horizontal"); 
         _vertical = Input.GetAxisRaw("Vertical");
+
+        // if Idle for (5s) transition toTHINKING
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Idle-MAIN")) {            
+            if (_isThinking) {
+                _isThinking = false;
+                _animator.ResetTrigger("toTHINKING");
+            }
+            if (!_isThinking) {
+                _idleTimer = _animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+            }            
+        }
+        if (_idleTimer > 5 && !_isThinking) {
+            _idleTimer = 0;
+            _isThinking = true;            
+            _animator.ResetTrigger("Exit");
+            _animator.SetTrigger("toTHINKING");
+        }
     }
 
     void FixedUpdate()
@@ -47,12 +67,11 @@ public class Rigidbody2dMovement : MonoBehaviour {
             // transition from idle to walk
 
             if (_body.velocity.x < 0 || _body.velocity.x > 0 || _body.velocity.y < 0 || _body.velocity.y > 0) {
-                _animator.SetTrigger("toWALK");
+                _animator.SetTrigger("toWALK");                
             } else if (_body.velocity.x == 0 || _body.velocity.y == 0) {
                 _animator.ResetTrigger("toWALK");
                 _animator.SetTrigger("Exit");
-            }
-
+            }            
         } else {
             _body.velocity = Vector2.zero;
         }
