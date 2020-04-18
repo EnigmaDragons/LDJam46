b298caee96b89d8bee4798dedd602fc82f890aac
noah reinagel <noah.r9@gmail.com>
2020-04-18 15:07:37 -0700

    picking up giant glasses

---
+++ src/LDJam46/Assets/Scripts/Inventory/ItemUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemUI : MonoBehaviour
+{
+    [SerializeField] private Image itemImage;
+
+    public void SetItem(Item item)
+    {
+        itemImage.sprite = item.Sprite;
+    }
+}
