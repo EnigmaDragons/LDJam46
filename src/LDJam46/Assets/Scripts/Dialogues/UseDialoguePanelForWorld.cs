d45103e0bdc5f7c6f0f1027c37c5494db37c6691
SilasReinagel <silas.reinagel@gmail.com>
2020-04-17 23:58:55 -0700

    Uses Dialogue Panel based on Current World. Closes #61

---
+++ src/LDJam46/Assets/Scripts/Dialogues/UseDialoguePanelForWorld.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UseDialoguePanelForWorld : MonoBehaviour
+{
+    [SerializeField] private CurrentGameState game;
+    [SerializeField] private Image image;
+    [SerializeField] private Sprite realWorldPanel;
+    [SerializeField] private Sprite mindWorldPanel;
+
+    private void OnEnable() => image.sprite = game.CurrentWorld == CurrentWorld.Real ? realWorldPanel : mindWorldPanel;
+}
