742ad1527f4f3b9421d532fab3716d8607c0ffe9
SilasReinagel <silas.reinagel@gmail.com>
2020-04-18 00:36:18 -0700

    Stores MainCharacter in CurrentPlayerCharacter. Closes #67

---
+++ src/LDJam46/Assets/Scripts/MainCharacter/CurrentPlayerCharacter.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "OnlyOnce/CurrentPlayerCharacter")]
+public class CurrentPlayerCharacter : ScriptableObject
+{
+    public GameObject PlayerCharacter;
+}
---
+++ src/LDJam46/Assets/Scripts/MainCharacter/SetCameraToFollowMainCharacter.cs
@@ -0,0 +1,10 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class SetCameraToFollowMainCharacter : MonoBehaviour
+{
+    [SerializeField] private CurrentPlayerCharacter character;
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+
+    void Start() => virtualCamera.Follow = character.PlayerCharacter.transform;
+}
