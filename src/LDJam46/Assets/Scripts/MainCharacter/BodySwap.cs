fc690c883dae8bdf95b32b29ad9f180a96b97333
noah reinagel <noah.r9@gmail.com>
2020-04-18 12:30:21 -0700

    body swapping now happens properply

---
+++ src/LDJam46/Assets/Scripts/MainCharacter/BodySwap.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BodySwap : OnMessage<WorldSwapStarted, WorldSwapPeaked>
+{
+    [SerializeField] private GameObject realBody;
+    [SerializeField] private GameObject mindBody;
+
+    private CurrentWorld _world;
+
+    protected override void Execute(WorldSwapStarted msg)
+    {
+        _world = msg.NewWorld;
+    }
+
+    protected override void Execute(WorldSwapPeaked msg)
+    {
+        if (_world == CurrentWorld.Real)
+        {
+            mindBody.SetActive(false);
+            realBody.SetActive(true);
+        }
+        else
+        {
+            realBody.SetActive(false);
+            mindBody.SetActive(true);
+        }
+    }
+}
