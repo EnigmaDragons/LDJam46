597e822c74201b6fb12e40e7356139e1e4851876
SilasReinagel <silas.reinagel@gmail.com>
2020-04-18 14:49:52 -0700

    Created 1 Tree. Closes #158

--- src/LDJam46/Assets/Scripts/Mind/MapGenerator.cs
+++ src/LDJam46/Assets/Scripts/Mind/MapGenerator.cs
@@ -4,10 +4,12 @@ public class MapGenerator : MonoBehaviour
 {
     [SerializeField] private int size;
     [SerializeField] private GroundSpawner ground;
+    [SerializeField] private TreeSpawner trees;
 
     public void OnEnable()
     {
         var sizeV = new Vector2Int(size, size);
         ground.Spawn(sizeV);
+        trees.Spawn(sizeV);
     }
 }
---
+++ src/LDJam46/Assets/Scripts/Mind/TreeSpawner.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class TreeSpawner : MonoBehaviour
+{
+    [SerializeField] private MapGeneratorObject[] objects;
+
+    public void Spawn(Vector2Int size)
+    {
+        foreach (Transform child in transform)
+            Destroy(child.gameObject);
+        
+        var iterator = new TwoDimensionalIterator(size.x, size.y);
+        iterator.ForEach(xy =>
+        {
+            foreach (var o in objects)
+                if (Rng.Dbl() * 100 < o.PercentChance)
+                {
+                    Instantiate(o.Object, new Vector3(xy.Item1, xy.Item2, 0), o.Object.transform.localRotation, transform);
+                    break;
+                }
+        });
+    }
+}
