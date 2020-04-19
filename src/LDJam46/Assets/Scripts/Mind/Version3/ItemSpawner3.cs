2aa8747f63155676ca05ff7784937cb03b329d2a
SilasReinagel <silas.reinagel@gmail.com>
2020-04-18 23:18:37 -0700

    Added Logging for Item Spawning

--- src/LDJam46/Assets/Scripts/Mind/Version3/ItemSpawner3.cs
+++ src/LDJam46/Assets/Scripts/Mind/Version3/ItemSpawner3.cs
@@ -8,6 +8,7 @@ public class ItemSpawner3 : MonoBehaviour
     
     public void Spawn(List<Vector3> possibleItemLocations)
     {
+        Debug.Log($"Number of Possible Item Locations {possibleItemLocations.Count}");
         foreach (Transform child in transform)
             Destroy(child.gameObject);
         
@@ -15,11 +16,13 @@ public class ItemSpawner3 : MonoBehaviour
         var index = 0;
         foreach (var rule in rules)
         {
+            Debug.Log($"Spawning {rule.NumberToSpawn} {rule.Object.name}(s)");
             for (var n = 0; n < rule.NumberToSpawn; n++)
             {
                 if (index < queue.Length)
                 {
                     Instantiate(rule.Object, queue[index], rule.Object.transform.localRotation, transform);
+                    Debug.Log($"Spawned {rule.Object.name} #{n}");
                     index++;
                 }
             }
