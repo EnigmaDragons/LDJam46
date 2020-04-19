b17242cedf25f41b750a3f6bd130b7714c2f6c9e
noah reinagel <noah.r9@gmail.com>
2020-04-19 00:54:41 -0700

    added sprinting

---
+++ src/LDJam46/Assets/Scripts/MainCharacter/SprintUI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SprintUI : OnMessage<SprintChanged>
+{
+    [SerializeField] private Slider sprintBar;
+    [SerializeField] private float secondsToFade;
+    [SerializeField] private Image background;
+    [SerializeField] private Image fill;
+
+    private bool _fading;
+
+    protected override void Execute(SprintChanged msg)
+    {
+        _fading = msg.SprintPercentage == 1;
+        sprintBar.value = msg.SprintPercentage;
+    }
+
+    private void Update()
+    {
+        if (!_fading)
+        {
+            fill.color = new Color(1, 1, 1, Math.Min(1, fill.color.a + Time.deltaTime / secondsToFade));
+            background.color = new Color(1, 1, 1, Math.Min(1, fill.color.a + Time.deltaTime / secondsToFade) / 2);
+        }
+        else
+        {
+            fill.color = new Color(1, 1, 1, Math.Min(1, fill.color.a - Time.deltaTime / secondsToFade));
+            background.color = new Color(1, 1, 1, Math.Min(1, fill.color.a - Time.deltaTime / secondsToFade) / 2);
+        }
+    }
+}
