e7a29296e319a8a2f1ff92dfbf9d1d12ecde14e8
Jaroslav Vozár <jarkovozar@gmail.com>
2020-04-19 14:33:42 +0200

    Pressing ESC (more specifically, virtual "Cancel" input) in Main Menu quits game; Closes #245

---
+++ src/LDJam46/Assets/Scripts/UI/MainMenuControls.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MainMenuControls : MonoBehaviour
+{
+	[SerializeField] private Navigator navigator;
+
+	void Update()
+    {
+	    if (Input.GetAxis("Cancel") > 0.001)
+	    {
+		    navigator.QuitGame();
+	    }
+    }
+}
