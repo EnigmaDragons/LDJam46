fa26fdebe7c911669ffb2c2ade0a36e8617bc50e
SilasReinagel <silas.reinagel@gmail.com>
2020-04-19 15:40:08 -0700

    First Mind World Dialogue. Closes #329

--- src/LDJam46/Assets/Scripts/Dialogues/OnStartBeginDialogue.cs
+++ src/LDJam46/Assets/Scripts/Dialogues/OnStartBeginDialogue.cs
@@ -1,9 +1,17 @@
 ﻿
+using System.Collections;
 using UnityEngine;
 
 public class OnStartBeginDialogue : MonoBehaviour
 {
     [SerializeField] private Dialogue dialogue;
-    
-    void Start() => Message.Publish(new StartConversation(dialogue));
+    [SerializeField] private FloatReference delaySeconds = new FloatReference(0f);
+
+    void Start() => StartCoroutine(BeginWithDelay());
+
+    private IEnumerator BeginWithDelay()
+    {
+        yield return new WaitForSeconds(delaySeconds);
+        Message.Publish(new StartConversation(dialogue));
+    }
 }
