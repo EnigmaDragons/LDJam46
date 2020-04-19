558901ef6a2a0d34899305a77c09c5b884e2d372
SilasReinagel <silas.reinagel@gmail.com>
2020-04-19 11:29:25 -0700

    Added Office Music. Tweaked UI Layers. Closes #287

--- src/LDJam46/Assets/Scripts/UI/GameReactiveUiText.cs
+++ src/LDJam46/Assets/Scripts/UI/GameReactiveUiText.cs
@@ -6,7 +6,12 @@ public abstract class GameReactiveUiText : OnMessage<GameStateChanged>
     [SerializeField] private CurrentGameState game;
     [SerializeField] private TextMeshProUGUI ui;
 
-    private void Start() => ui.text = GetValue(game.GameState);
+    private void Start()
+    {
+        if (game.GameState == null)
+            return;
+        ui.text = GetValue(game.GameState);
+    }
 
     protected abstract string GetValue(GameState game);
     protected override void Execute(GameStateChanged msg) => ui.text = GetValue(msg.State);
