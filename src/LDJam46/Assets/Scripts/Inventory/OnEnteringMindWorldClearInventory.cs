a0127fc78d6236289c0c4a3cf156a5c27efcd6c8
SilasReinagel <silas.reinagel@gmail.com>
2020-04-19 02:06:57 -0700

    Keeps Pills On Traveling to Mind World. Closes #234

--- src/LDJam46/Assets/Scripts/Inventory/OnEnteringMindWorldClearInventory.cs
+++ src/LDJam46/Assets/Scripts/Inventory/OnEnteringMindWorldClearInventory.cs
@@ -1,13 +1,14 @@
-﻿
+﻿using System.Linq;
 using UnityEngine;
 
 public class OnEnteringMindWorldClearInventory : OnMessage<WorldSwapStarted>
 {
     [SerializeField] private CurrentGameState game;
+    [SerializeField] private string[] persistentItems = new string[1] { "Pills" };
     
     protected override void Execute(WorldSwapStarted msg)
     {
         if (msg.NewWorld == CurrentWorld.Mind)
-            game.UpdateState(gs => gs.Items.Clear());
+            game.UpdateState(gs => gs.Items = gs.Items.Where(i => persistentItems.Contains(i.Name)).ToList());
     }
 }
