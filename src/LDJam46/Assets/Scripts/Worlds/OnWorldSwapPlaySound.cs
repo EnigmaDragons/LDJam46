8106734d7c7649805868412da168a31c1480d939
SilasReinagel <silas.reinagel@gmail.com>
2020-04-19 00:54:34 -0700

    World swap sound. Closes #219

---
+++ src/LDJam46/Assets/Scripts/Worlds/OnWorldSwapPlaySound.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class OnWorldSwapPlaySound : OnMessage<WorldSwapStarted>
+{
+    [SerializeField] private UiSfxPlayer uiSounds;
+    [SerializeField] private AudioClip clip;
+    [SerializeField] private float volume = 0.5f;
+
+    protected override void Execute(WorldSwapStarted msg)
+    {
+        uiSounds.Play(clip, volume);
+    }
+}
