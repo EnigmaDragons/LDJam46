57dd3cf29146767834eab37a4da3bdb1f8555efc
SilasReinagel <silas.reinagel@gmail.com>
2020-04-19 16:19:38 -0700

    Created Player Thought UI

--- src/LDJam46/Assets/Scripts/UI/ShowThoughtLineUI.cs
+++ src/LDJam46/Assets/Scripts/UI/ShowThoughtLineUI.cs
@@ -1,30 +1,37 @@
-﻿using System;
+﻿
+using System;
 using TMPro;
 using UnityEngine;
 
-public class DialogueLineUI : MonoBehaviour
+public class ShowThoughtLineUI : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI textBox;
+    [SerializeField] private TextMeshPro textBox;
     [SerializeField] private FloatReference secondsPerCharacter;
 
     private string _text = "";
     private float _t;
+    private Action _onFinished;
 
-    private void OnEnable() => textBox.text = "";
-    
     public bool IsRevealed => _t >= _text.Length * secondsPerCharacter;
     public void Reveal() => _t = _text.Length * secondsPerCharacter;
 
-    public void Display(string text)
+    public void Display(string text, Action onFinished)
     {
         textBox.text = "";
+        gameObject.SetActive(true);
+        _onFinished = onFinished;
         _text = text;
         _t = 0;
     }
 
-    public void ManualUpdate()
+    private void Update()
     {
+        if (IsRevealed)
+            return;
+        
         _t += Time.deltaTime;
         textBox.text = _text.Substring(0, (int) Math.Min(_text.Length, Math.Floor(_t / secondsPerCharacter)));
+        if (IsRevealed)
+            _onFinished();
     }
 }
