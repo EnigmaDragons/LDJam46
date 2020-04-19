d824ccd070be9a2ca70e107c23cd116ac81a432c
SilasReinagel <silas.reinagel@gmail.com>
2020-04-18 23:12:03 -0700

    Item Spawning

---
+++ src/LDJam46/Assets/Scripts/Mind/Version3/ItemSpawner3.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner3 : MonoBehaviour
+{
+    [SerializeField] private ItemSpawnRule[] rules;
+    
+    public void Spawn(List<Vector3> possibleItemLocations)
+    {
+        foreach (Transform child in transform)
+            Destroy(child.gameObject);
+        
+        var queue = possibleItemLocations.ToArray().Shuffled();
+        var index = 0;
+        foreach (var rule in rules)
+        {
+            for (var n = 0; n < rule.NumberToSpawn; n++)
+            {
+                if (index < queue.Length)
+                {
+                    Instantiate(rule.Object, queue[index], rule.Object.transform.localRotation, transform);
+                    index++;
+                }
+            }
+
+        }
+    }
+}
---
+++ src/LDJam46/Assets/Scripts/Mind/Version3/OnEnableRecordItemSpawnPoint.cs
@@ -0,0 +1,7 @@
+﻿
+using UnityEngine;
+
+public class OnEnableRecordItemSpawnPoint : MonoBehaviour
+{
+    private void OnEnable() => Message.Publish(new RecordItemSpawnPoint(transform.position));
+}
