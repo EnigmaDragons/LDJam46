50b2935d0f16e7ed72589a370d18578d04de8bf5
SilasReinagel <silas.reinagel@gmail.com>
2020-04-18 18:09:57 -0700

    Added foliage spawning. Closes #186

---
+++ src/LDJam46/Assets/Scripts/Mind/GrassSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GrassSpawner : MonoBehaviour, ISpawner
+{
+    [SerializeField, Range(0, 100)] private float density;
+    [SerializeField] private GameObject[] foliage;
+    
+    public void Spawn(Vector2Int size)
+    {
+        foreach (Transform child in transform)
+            Destroy(child.gameObject);
+        
+        var iterator = new TwoDimensionalIterator(size.x, size.y);
+        iterator.ForEach(xy =>
+        {
+            if (Rng.Dbl() * 100 < density)
+            {
+                var o = foliage.Random();
+                Instantiate(o, new Vector3(xy.Item1, xy.Item2, 0), o.transform.localRotation, transform);
+            }
+        });
+    }
+}
