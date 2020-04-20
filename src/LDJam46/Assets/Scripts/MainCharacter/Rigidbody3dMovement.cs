6f083541aac528a071287b405dc5cdeb4ddeba00
SilasReinagel <silas.reinagel@gmail.com>
2020-04-20 00:08:42 -0700

    Idles when In Conversation. Closes #181

--- src/LDJam46/Assets/Scripts/MainCharacter/Rigidbody3dMovement.cs
+++ src/LDJam46/Assets/Scripts/MainCharacter/Rigidbody3dMovement.cs
@@ -33,7 +33,7 @@ public class Rigidbody3dMovement : MonoBehaviour {
     {
         _horizontal = Input.GetAxisRaw("Horizontal"); 
         _vertical = Input.GetAxisRaw("Vertical");
-
+        
         // if Idle for (5s) transition toTHINKING
         if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Idle-MAIN")) {            
             if (_isThinking) {
@@ -44,6 +44,7 @@ public class Rigidbody3dMovement : MonoBehaviour {
                 _idleTimer = _animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
             }            
         }
+
         if (_idleTimer > 5 && !_isThinking) {
             _idleTimer = 0;
             _isThinking = true;            
@@ -88,6 +89,7 @@ public class Rigidbody3dMovement : MonoBehaviour {
             }
         } else {
             _body.velocity = Vector2.zero;
+            Idle();
         }
     }
 
