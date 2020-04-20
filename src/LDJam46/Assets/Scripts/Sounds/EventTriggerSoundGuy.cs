248efb0e9497265d993a409014edd3973347bac0
SilasReinagel <silas.reinagel@gmail.com>
2020-04-20 03:17:31 -0700

    Plays Item Gained Sound. Closes #270

---
+++ src/LDJam46/Assets/Scripts/Sounds/EventTriggerSoundGuy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class EventTriggerSoundGuy : MonoBehaviour
+{
+    [SerializeField] private UiSfxPlayer player;
+    [SerializeField] private AudioClipWithVolume itemCollected;
+    
+    private void OnEnable()
+    {
+        Message.Subscribe<GainItem>(_ => PlayItemCollected(), this);
+    }
+
+    private void OnDisable() => Message.Unsubscribe(this);
+
+    private void PlayItemCollected() => player.Play(itemCollected.Clip, itemCollected.Volume);
+}
