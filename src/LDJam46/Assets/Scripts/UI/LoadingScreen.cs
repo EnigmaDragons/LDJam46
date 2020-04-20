e3f9a58b6e0579af1ea38d0dafa0a1b153acefa2
Jaroslav Vozár <jarkovozar@gmail.com>
2020-04-20 17:42:45 +0200

    Added text and mandatory 3.5 second wait for Loading Screen; Closes #415; Closes #414

--- src/LDJam46/Assets/Scripts/UI/LoadingScreen.cs
+++ src/LDJam46/Assets/Scripts/UI/LoadingScreen.cs
@@ -9,6 +9,8 @@ public class LoadingScreen : MonoBehaviour
 	[SerializeField] private GameObject forHiding;
 	[SerializeField] private Animator backgroundAnimator;
 
+	private bool _mandatoryWaitOver = false;
+
 	private void Start()
 	{
 		forHiding.SetActive(false);
@@ -22,18 +24,32 @@ public class LoadingScreen : MonoBehaviour
 
 	public void LoadScene(string sceneName)
 	{
+		StartCoroutine(MandatoryWait());
 		StartCoroutine(LoadSceneAsync(sceneName));
 	}
 
-	IEnumerator LoadSceneAsync(string sceneName)
+	private IEnumerator MandatoryWait()
+	{
+		yield return new WaitForSeconds(3.5f);
+
+		_mandatoryWaitOver = true;
+	}
+
+	private IEnumerator LoadSceneAsync(string sceneName)
     {
 	    AsyncOperation sceneLoadOperation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+		sceneLoadOperation.allowSceneActivation = false;
 
 	    while (!sceneLoadOperation.isDone)
 	    {
 		    //Debug.Log("LOAD SCENE: Progress: " + sceneLoadOperation.progress);
 
-			loadingSlider.value = Mathf.Clamp01(sceneLoadOperation.progress / 0.9f);
+			loadingSlider.value = sceneLoadOperation.progress;
+
+			if (_mandatoryWaitOver && sceneLoadOperation.progress >= 0.89f)
+			{
+				sceneLoadOperation.allowSceneActivation = true;
+			}
 
 		    yield return null;
 	    }
