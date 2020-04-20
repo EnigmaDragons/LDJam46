0891b24a232d31063736d0a0770abe346ffb319a
SilasReinagel <silas.reinagel@gmail.com>
2020-04-19 20:02:07 -0700

    Journal View interaction improve. Closes #355

---
+++ src/LDJam46/Assets/Scripts/UI/CannotBeActiveDuringDialogue.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class CannotBeActiveDuringDialogue : MonoBehaviour
+{
+    [SerializeField] private CurrentGameState game;
+
+    void OnEnable()
+    {
+        if (game.GameState.isInDialogue)
+        {
+            Debug.Log($"{gameObject.name} cannot be active while Player Is In Dialogue", gameObject);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ src/LDJam46/Assets/Scripts/UI/JournalView.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class JournalView : MonoBehaviour
+{
+    [SerializeField] private GameObject journalView;
+    
+    public void Toggle()
+    {
+        if (journalView.activeSelf)
+            Hide();
+        else
+            Show();
+    }
+    
+    public void Show()
+    {
+        journalView.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        journalView.SetActive(false);
+    }
+}
