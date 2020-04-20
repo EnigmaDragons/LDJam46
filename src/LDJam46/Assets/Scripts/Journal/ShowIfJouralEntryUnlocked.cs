449b8895c3a1e38a70c00ce621c0c6ffb92eb363
SilasReinagel <silas.reinagel@gmail.com>
2020-04-19 20:23:36 -0700

    Setup Journal Unlocking. Closes #242

---
+++ src/LDJam46/Assets/Scripts/Journal/ShowIfJouralEntryUnlocked.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class ShowIfJouralEntryUnlocked : MonoBehaviour
+{
+    [SerializeField] private JournalEntry entry;
+    [SerializeField] private CurrentGameState game;
+
+    private void OnEnable()
+    {
+        if(!game.GameState.UnlockedJournalEntries.Contains(entry))
+            gameObject.SetActive(false);
+    }
+}
