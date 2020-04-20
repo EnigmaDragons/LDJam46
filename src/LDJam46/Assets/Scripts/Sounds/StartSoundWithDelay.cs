ff3abb5d1a0f41ecee4e2456a2dcc79740b07f98
SilasReinagel <silas.reinagel@gmail.com>
2020-04-20 02:45:04 -0700

    Added Office Sounds. Closes #375

---
+++ src/LDJam46/Assets/Scripts/Sounds/StartSoundWithDelay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class StartSoundWithDelay : MonoBehaviour
+{
+    [SerializeField] private float delay;
+
+    private void OnEnable() => StartCoroutine(Execute(GetComponent<AudioSource>()));
+
+    private IEnumerator Execute(AudioSource src)
+    {
+        yield return new WaitForSeconds(delay);
+        src.Play();
+    }
+}
