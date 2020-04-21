9474caf16d396695fa0dde0abe88e37da648b533
SilasReinagel <silas.reinagel@gmail.com>
2020-04-21 15:42:13 -0700

    Added Music Comfort Fanfare. Closes #499

--- src/LDJam46/Assets/Scripts/Sounds/EventTriggerSoundGuy.cs
+++ src/LDJam46/Assets/Scripts/Sounds/EventTriggerSoundGuy.cs
@@ -4,13 +4,24 @@ public class EventTriggerSoundGuy : MonoBehaviour
 {
     [SerializeField] private UiSfxPlayer player;
     [SerializeField] private AudioClipWithVolume itemCollected;
+    [SerializeField] private AudioClipWithVolume[] headphonesFanfares;
     
     private void OnEnable()
     {
         Message.Subscribe<GainItem>(_ => PlayItemCollected(), this);
+        Message.Subscribe<ComfortConsumed>(msg => PlayComfortConsumedSound(msg), this);
     }
 
     private void OnDisable() => Message.Unsubscribe(this);
 
-    private void PlayItemCollected() => player.Play(itemCollected.Clip, itemCollected.Volume);
+    private void PlayComfortConsumedSound(ComfortConsumed c)
+    {
+        if (c.Comfort.JournalEntry == JournalEntry.ComfortMusic)
+            PlayHeadphonesFanfare();
+    }
+    
+    private void PlayItemCollected() => Play(itemCollected);
+    private void PlayHeadphonesFanfare() => Play(headphonesFanfares.Random());
+
+    private void Play(AudioClipWithVolume a) => player.Play(a.Clip, a.Volume);
 }
