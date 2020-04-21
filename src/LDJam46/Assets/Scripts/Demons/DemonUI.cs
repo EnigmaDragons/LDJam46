a3307068ba4b8a67bda9212e34c1cb042ae6819d
noah reinagel <noah.r9@gmail.com>
2020-04-20 19:07:16 -0700

    add demon ui

---
+++ src/LDJam46/Assets/Scripts/Demons/DemonUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DemonUI : MonoBehaviour
+{
+    [SerializeField] private DemonState demonState;
+    [SerializeField] private Slider slider;
+
+    private void Update()
+    {
+        gameObject.SetActive(demonState.IsActive);
+        slider.value = demonState.ProgressPercent;
+    }
+}
