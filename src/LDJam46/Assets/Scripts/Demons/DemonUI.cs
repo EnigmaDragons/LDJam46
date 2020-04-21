c5a8890937116e805aa2bd32d9b472ca59ebc514
SilasReinagel <silas.reinagel@gmail.com>
2020-04-21 16:22:03 -0700

    Updated Demon UI. Closes #508

--- src/LDJam46/Assets/Scripts/Demons/DemonUI.cs
+++ src/LDJam46/Assets/Scripts/Demons/DemonUI.cs
@@ -6,6 +6,8 @@ public class DemonUI : MonoBehaviour
     [SerializeField] private DemonState demonState;
     [SerializeField] private Slider slider;
 
+    private void Awake() => gameObject.SetActive(false);
+    
     private void Update()
     {
         gameObject.SetActive(demonState.IsActive);
