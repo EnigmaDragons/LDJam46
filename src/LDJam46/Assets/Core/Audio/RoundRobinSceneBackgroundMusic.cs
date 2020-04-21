f3178b026f24c6323a28711ff927735c4e45b63f
SilasReinagel <silas.reinagel@gmail.com>
2020-04-21 16:10:18 -0700

    Round Robin Forest Music. Closes #506

---
+++ src/LDJam46/Assets/Core/Audio/RoundRobinSceneBackgroundMusic.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public sealed class RoundRobinSceneBackgroundMusic : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] musics;
+    [SerializeField] private GameMusicPlayer musicPlayer;
+    [SerializeField] private bool shouldLoop = true;
+
+    private bool _shouldPlay;
+    private IndexSelector<AudioClip> _musics;
+
+    private void Awake() => _musics = new IndexSelector<AudioClip>(musics);
+    
+    private void OnEnable() => _shouldPlay = true;
+    private void OnDisable() => _shouldPlay = false;
+    
+    private void Update()
+    {
+        if (!_shouldPlay)
+            return;
+
+        _shouldPlay = false;
+        if (shouldLoop)
+            musicPlayer.PlaySelectedMusicLooping(_musics.MoveNext());
+        else 
+            musicPlayer.PlayMusicOnce(_musics.MoveNext());
+    }
+}
