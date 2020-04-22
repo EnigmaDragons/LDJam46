8f4d05afe7c141ecaddda80020b0633c1ebc5ff9
SilasReinagel <silas.reinagel@gmail.com>
2020-04-21 19:21:39 -0700

    Fix truncated text. Closes #510

---
+++ src/LDJam46/Assets/Scripts/Development/StartDialogue.cs
@@ -0,0 +1,9 @@
+﻿
+using UnityEngine;
+
+public class StartDialogue : MonoBehaviour
+{
+    [SerializeField] private Dialogue dialogue;
+    
+    public void Execute() => Message.Publish(new StartConversation(dialogue));
+}
