51c69c9982f9543d0c5a8a67ff1e5a04e62e5558
SilasReinagel <silas.reinagel@gmail.com>
2020-04-21 19:05:31 -0700

    Office Sounds Wait until after game over dismissed. Closes #520

---
+++ src/LDJam46/Assets/Scripts/State/EnableTargetAfterGameOverScreen.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+public class EnableTargetAfterGameOverScreen : OnMessage<GameStateChanged>
+{
+    [SerializeField] private GameObject target;
+    [SerializeField] private CurrentGameState game;
+
+    private bool _shouldEnable;
+    
+    private void OnEnable()
+    {
+        if (game.GameState.IsShowingGameOverScreen)
+        {
+            target.SetActive(false);
+            _shouldEnable = true;
+        }
+        else
+            target.SetActive(true);
+    }
+
+    private void OnDisable() => target.SetActive(false);
+
+    protected override void Execute(GameStateChanged msg)
+    {
+        if (!_shouldEnable || msg.State.IsShowingGameOverScreen)
+            return;
+
+        _shouldEnable = false;
+        target.SetActive(true);
+    }
+}
